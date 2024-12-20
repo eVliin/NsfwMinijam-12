41c16f3f134b7c52eb572feb29a69164dd78f6ef
eVliin <eVliin@catvalk.online>
2024-12-20 17:27:08 -0300

    Testdialog

--- Autoload/Global.gd
+++ Autoload/Global.gd
@@ -5,6 +5,7 @@ const TEST_ROOM = "res://Scenes/Rooms/TestRoom/TestRoom.tscn"
 const HUD = "res://Scenes/HUD/Hud.tscn"
 
 var game_controller : GameController
+var player_has_control : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Scenes/Entities/Present/present.gd
+++ Scenes/Entities/Present/present.gd
@@ -6,11 +6,14 @@ const BLUE = preload("res://Assets/PLaceholder/present/texture.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Dialogic.start('TestDialogue')
 	match color:
 		"Blue":
 			$Sprite2D.texture = BLUE
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event && Input.is_action_just_pressed("dialogic_default_action"):
-		SignalBus.present_open.emit(name)
-		SignalBus.pop_open.emit()
+	if Global.player_has_control:
+		print("opened")
+		if event && Input.is_action_just_pressed("Select"):
+			SignalBus.present_open.emit(name)
+			SignalBus.pop_open.emit()
