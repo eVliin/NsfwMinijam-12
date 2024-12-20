41a699fb764090c8036162f5423e32b4eb403417
eVliin <eVliin@catvalk.online>
2024-12-20 14:15:14 -0300

    scene manager

--- Autoload/Global.gd
+++ Autoload/Global.gd
@@ -1,11 +1,11 @@
 extends Node
 
+const MAIN_MENU = "res://Scenes/Menus/Main Menu/MainMenu.tscn"
+const TEST_ROOM = "res://Scenes/Rooms/TestRoom/TestRoom.tscn"
+const HUD = "res://Scenes/HUD/Hud.tscn"
+
+var game_controller : GameController
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- Scenes/Entities/Present/present.gd
+++ Scenes/Entities/Present/present.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 		"Blue":
 			$Sprite2D.texture = BLUE
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event && Input.is_action_just_pressed("dialogic_default_action"):
 		SignalBus.present_open.emit(name)
 		SignalBus.pop_open.emit()
